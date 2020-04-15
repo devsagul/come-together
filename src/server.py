0b1d0deb81dc19dceb9921f0adf9a5b66ef05ae6
S. Guliaev <dev.sagul@gmail.com>
2020-04-15 13:27:50 +0300

    add basic uploading logic to server

--- src/server.py
+++ src/server.py
@@ -1,23 +1,26 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template, request
+from gluer import glue
 
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    """Render a demopage for the service"""
-    return "Hello World!"
+    """Render a demopage with form for the service"""
+    return render_template('index.html', name="Come Together")
 
 @app.route("/cat", methods=["POST"])
 def concatenate():
     """Concatenate several images / pdf documents into one"""
-    scale = request.args.scale('scale', 'up')
-    fmt = request.args.scale('format', 'pdf')
+    scale = request.values.get('scale', 'up')
+    fmt = request.values.get('format', 'pdf')
+    files = request.files.getlist("file[]")
+    # valudate file types
     try:
         result = Gluer.glue(files, scale=scale, fmt=fmt)
     except Exception:
         ...
-    return send_file(result.fp,
+    return send_file(result.file,
                      attachment_filename=result.display_name,
                      mimetype=result.mimetype)
 

e58042caa2de9914ca680616bf84b2bc43fc0c94
S. Guliaev <dev.sagul@gmail.com>
2020-04-15 17:05:38 +0300

    mark harcoded value

--- src/gluer.py
+++ src/gluer.py
@@ -1,33 +1,36 @@
 import os
 import PyPDF2
 import io
+import math
+import pdf2image
 from fpdf import FPDF
 from werkzeug.datastructures import FileStorage
 from PIL import Image
-from tempfile import TemporaryFile, NamedTemporaryFile
+from tempfile import NamedTemporaryFile
 
 
 class GlueResult:
     def __init__(self, scale, fmt):
         if scale not in ["up", "down"]:
-            raise ValueError("...")
+            raise ValueError('scale has to be up or down')
         if fmt not in ["pdf", "jpg", "png"]:
-            raise ValueError("...")
+            raise ValueError('fmt has to be one of pdf, jpg, png')
         self.scale = scale
         self.fmt = fmt
         self.width = None
-        self.tmpfile = TemporaryFile()
+        self.tmpfile = NamedTemporaryFile(suffix="."+fmt)
         self.files = []
 
     def __add__(self, f):
         if not isinstance(f, FileStorage):
-            raise TypeError("...")
+            raise TypeError('f has to be an instance of FileStorage')
         self.files.append(f)
         if f.mimetype.startswith('image'):
             width = Image.open(f).size[0]
-            func = min if self.scale == 'down' else max
-            if self.width is not None: self.width = func(self.width, width)
-            else: self.width = width
+        else: width = 2200 # TODO remove hardcoded value 
+        func = min if self.scale == 'down' else max
+        if self.width is not None: self.width = func(self.width, width)
+        else: self.width = width
         return self
     
     @property
@@ -45,12 +48,12 @@ class GlueResult:
 
     @property
     def file(self):
-        
-        # write all the files
-        # converting and scaling them
         if self.fmt == 'pdf':
             image_sources = [self.convert_to_pdf(item) for item in self.files]
-        else: image_sources = [self.convert_to_image(item) for item in self.files]
+        else: 
+            image_sources =[]
+            for item in self.files:
+                image_sources += self.convert_to_images(item)
         self._concat(image_sources)
         self.tmpfile.seek(0, os.SEEK_SET)
         return self.tmpfile
@@ -64,37 +67,17 @@ class GlueResult:
             merger.write(self.tmpfile)
             merger.close()
         else:
-            ...
-
-    @staticmethod
-    def concat_images(images):
-        shapes = (item.shape[:2] for item in images)
-        hs, ws = zip(*shapes)
-        ws = list(ws)
-        w = max(ws)
-        ratios = (w / item for item in ws)
-        h = int(max(hs) * max(ratios) * len(images))
-        res = np.zeros((h, w, 3), np.uint8)
-        s = 0
-        for img in images:
-            cur_h, cur_w = img.shape[:2]
-            ratio = w / cur_w
-            resized = cv2.resize(img, None, fx=ratio, fy=ratio)
-            res[s:s+resized.shape[0], :w, :3] = resized
-            s += resized.shape[0]
-        return res[:s]
+            dst = Image.new('RGB', (self.width, sum([x.height for x in sources])))
+            h = 0
+            for image in sources:
+                dst.paste(image, (0, h))
+                h += image.height
+            dst.save(self.tmpfile)
 
     @staticmethod
     def convert_to_pdf(fp):
         if not isinstance(fp, FileStorage):
-            raise TypeError('...')
-        if (fp.mimetype.endswith('pdf')):
-            return fp
-        if (fp.mimetype.startswith('image')):
-            pdf = FPDF()
-            pdf.add_page()    def convert_to_pdf(fp):
-        if not isinstance(fp, FileStorage):
-            raise TypeError('...')
+            raise TypeError('fp has to be an instance of FileStorage')
         if (fp.mimetype.endswith('pdf')):
             return fp
         if (fp.mimetype.startswith('image')):
@@ -108,28 +91,28 @@ class GlueResult:
                 pdf.image(tmp.name, w = 190)
             else:
                 pdf.image(tmp.name, h = 257)
-            return io.BytesIO(pdf.outpu
-            image = Image.open(fp)
-            tmp = NamedTemporaryFile(suffix='.png')
-            w, h = image.size
-            image.save(tmp)
-            if w / h > 190 / 257:
-                pdf.image(tmp.name, w = 190)
-            else:
-                pdf.image(tmp.name, h = 257)
             return io.BytesIO(pdf.output(dest='S').encode('latin-1'))
-        raise ValueError('...')
+        raise ValueError(f'Incopatible mimetype: {fp.mimetype}')
 
-    @staticmethod
-    def convert_to_image(fp):
+    def convert_to_images(self, fp):
         if not isinstance(fp, FileStorage):
-            raise TypeError('...')
+            raise TypeError('fp has to be an instance of FileStorage')
         if (fp.mimetype.startswith('image')):
-            return Image.open(fp)
+            image = Image.open(fp)
+            return [scale_image(image, self.width)]
         if (fp.mimetype.endswith('pdf')):
-            ...
-        raise ValueError('...')
+            images = pdf2image.convert_from_bytes(fp.read(), dpi=300)
+            return [scale_image(image, self.width) for image in images]
 
+        raise ValueError(f'Incopatible mimetype: {fp.mimetype}')
+
+
+def scale_image(image, width):
+    scale = width / image.width
+    i = image.resize((math.ceil(scale * image.width),
+                        math.ceil(scale * image.height)),
+                        Image.ANTIALIAS)
+    return i
 
 def glue(files, scale, fmt):
     return sum(files, start=GlueResult(scale, fmt))
